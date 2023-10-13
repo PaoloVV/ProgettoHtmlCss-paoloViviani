e572fc527b685b6ab9c181d59438de816b5abcde
PaoloVV <118202060+PaoloVV@users.noreply.github.com>
2023-10-13 18:16:18 +0200

    new update

--- assets/app.js
+++ assets/app.js
@@ -8,12 +8,32 @@ let pAlertMessageOk = document.createTextNode("IL MESSAGGIO E' STATO INVIATO COR
 let pAlertMessageNo = document.createTextNode("MESSAGGIO NON INVIATO. RIPROVARE")
 let header = document.getElementById("navbar")
 
-pAlertMessageOk
+const submitBtn = document.querySelector(".submit-btn")
 
+function attivaSubmit(){
+    let params = {
+        nome: document.getElementById("nome").value,
+        email: document.getElementById("email").value,
+        messaggio: document.getElementById("messaggio").value
+    }
+    const errorMessage = document.getElementById("error-message")
+    const successMessage = document.getElementById("success-message")
+
+    if(params.nome !== "" && params.email !== "" && params.messaggio !== ""){
+        submitBtn.disabled = false
+        console.log("button ok")
+        successMessage.innerHTML = "Ok!"
+        errorMessage.innerHTML = ""
+    } else {
+        submitBtn.disabled = true
+        console.log("button disabled")
+        successMessage.innerHTML = ""
+        errorMessage.innerHTML = "Compila tutti i campi"
+        }
+}
 
 
 function sendMail(){
-    console.log("wewe")
     let params = {
         nome: document.getElementById("nome").value,
         email: document.getElementById("email").value,
@@ -22,24 +42,25 @@ function sendMail(){
 
     const serviceId = "service_portfolio"
     const templateId = "template_pggsa0f"
-
+    
     emailjs.send(serviceId, templateId, params)
-    .then(function (res){
-        alert("Success")
-    })
-    .catch(error => alert("compilare tutti i campi"))
-}
-
-window.onscroll = function(){
-    if(document.body.scrollTop >= 10 || document.documentElement.scrollTop >= 10){
-        console.log("scrolling")
-        header.classList.add("header-scrolled")
-        header.classList.remove("header-no-scrolled")
-    } else{
-        header.classList.add("header-no-scrolled")
-        header.classList.remove("header-scrolled")
+        .then(function (res){
+            console.log("messaggio inviato")
+        })
+        .catch(error => alert("compilare tutti i campi"))
     }
-}
+
+
+    window.onscroll = function(){
+        if(document.body.scrollTop >= 10 || document.documentElement.scrollTop >= 10){
+            console.log("scrolling")
+            header.classList.add("header-scrolled")
+            header.classList.remove("header-no-scrolled")
+        } else{
+            header.classList.add("header-no-scrolled")
+            header.classList.remove("header-scrolled")
+        }
+    }
 
 
 
