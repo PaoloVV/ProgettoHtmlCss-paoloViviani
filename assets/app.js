6ae0d1b963879bd1066dc5d5c85e911ccbb7e52a
PaoloVV <118202060+PaoloVV@users.noreply.github.com>
2023-10-15 12:15:26 +0200

    Final Release

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,6 @@
-// let navLink = document.querySelectorAll(".nav-link")
-// let alertOk = getElementById("alert-message-ok")
-// let alertMessageNo = getElementById("alert-message-no")
-
-const divAlert = document.getElementById("div-alert")
-let pAlert = document.getElementById("p-alert")
-let pAlertMessageOk = document.createTextNode("IL MESSAGGIO E' STATO INVIATO CORRETTAMENTE")
-let pAlertMessageNo = document.createTextNode("MESSAGGIO NON INVIATO. RIPROVARE")
 let header = document.getElementById("navbar")
-
 const submitBtn = document.querySelector(".submit-btn")
+
 
 function attivaSubmit(){
     let params = {
@@ -62,29 +54,5 @@ function sendMail(){
         }
     }
 
-
-
-// let section2 = document.getElementsByClassName("section2");
-// let section3 = document.getElementsByClassName("section3")
-
-// console.log(section2)
-
-// //INTERSECTION OBSERVER
-// const sections = document.querySelectorAll(".section")
-// const observer = new IntersectionObserver(entries =>{
-//     entries.forEach(entry=>{
-//         entry.target.classList.toggle("show", entry.isIntersecting)
-//         if(entry.isIntersecting) observer.unobserve(entry.target)
-//     })
-//     console.log(entries)
-// },
-// {threshold: 0.35})
-
-// sections.forEach(section =>{
-//     observer.observe(section)
-// })
-
-// const inputEmail = document.getElementById("form-input-email")
-// const inputTextarea = document.getElementById("form-input-textarea")
 
 
